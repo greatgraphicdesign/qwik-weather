c12548415b8d775b9038a5c488ff724a9eab8a2a
Alek Vila <alekvila@gmail.com>
2024-05-21 14:51:23 -0700

    move getWeather to a hook file and rename to useWeather

--- src/hooks/useWeather.tsx
+++ src/hooks/useWeather.tsx
@@ -1,33 +1,76 @@
-import {useContext} from '@builder.io/qwik';
+import type {GeocodingObjType} from '~/types/apis';
+import type {ErrorResponse, SuccessResponse, WeatherResource} from '~/routes';
 
-import {WeatherContext} from '~/routes';
-import type {WeatherContextType} from '~/routes';
-
-export const useWeather = (query: string) => {
-  const weatherContextObj = useContext(WeatherContext) as WeatherContextType;
+export async function useWeather(
+  query: string,
+  controller: AbortController
+): Promise<WeatherResource> {
+  function convertToFlag(countryCode: string) {
+    const codePoints = countryCode
+      .toUpperCase()
+      .split('')
+      .map((char) => 127397 + char.charCodeAt(0));
+    return String.fromCodePoint(...codePoints);
+  }
 
   try {
-    if (query.length < 4) return;
+    const data = {
+      displayLocation: '',
+      forecast: {},
+      weatherToday: {},
+    };
 
-    if (/,\s/.test(query)) {
-      alert('space in the name');
-      // throw new Error(`Please enter the city name (only) or a zip code.`);
+    if (/,\s/.test(query) && query.length < 4) {
+      throw new Error(`Please enter the city name (only) or a zip code.`);
     }
 
+    // 1) Get Geo Location
+    const res = await fetch(
+      `https://geocoding-api.open-meteo.com/v1/search?name=${query}&count=10&language=en&format=json`,
+      {signal: controller.signal}
+    );
+    if (!res.ok) {
+      throw new Error('Something went wrong when fetching the geo location.');
+    }
+    const geoJSON: GeocodingObjType = await res.json();
+
+    // Open Meteo specific failure state
+    if (!geoJSON.results) {
+      throw new Error(`“${query}” not found.`);
+    }
+
+    const {latitude, longitude, timezone, name, admin1, country_code} =
+      geoJSON.results[0];
+
+    data.displayLocation = `${name}${admin1 ? `, ${admin1}` : ''} ${convertToFlag(country_code)}`;
+
+    // 2) Get forecast
+    const forecastRes = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&temperature_unit=fahrenheit&forecast_days=8&daily=weathercode,temperature_2m_max,temperature_2m_min`,
+      {signal: controller.signal}
+    );
+    if (!forecastRes.ok) {
+      throw new Error('Something went wrong when fetching the forecast.');
+    }
+    const forecastData = await forecastRes.json();
+    data.forecast = forecastData.daily;
+
+    // 3) Get today's weather
+    const todayRes = await fetch(
+      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&temperature_unit=fahrenheit&forecast_days=2&hourly=weathercode,temperature_2m,temperature_2m&daily=temperature_2m_max,temperature_2m_min`,
+      {signal: controller.signal}
+    );
+    if (!todayRes.ok) {
+      throw new Error('Something went wrong when fetching the forecast.');
+    }
+    const todayData = await todayRes.json();
+    data.weatherToday = todayData;
+
+    console.log('data', data);
     console.log('calling', query);
 
-    // const resp = await fetch('http://localhost:5173/api/beers', {
-    //   signal: controller?.signal,
-    // });
-    // const json = await resp.json();
-    // return json;
-
-    // const res = await fetch(`http://weatherdata.com?city=${query}`, {
-    //   signal: abortController.signal,
-    // });
-    // const data = await res.json();
-    // return data as {temp: number};
+    return data as SuccessResponse;
   } catch (error) {
-    weatherContextObj.error = error.message;
+    return error as ErrorResponse;
   }
-};
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -15,14 +15,10 @@ import {Error} from '~/components/error';
 import {Loader} from '~/components/loader';
 import {Input} from '~/components/input';
 import {WeatherLayout} from '~/components/weather';
-
+import {useWeather} from '~/hooks/useWeather';
 import {PortalAPI} from '~/portal-provider';
 
-import type {
-  GeocodingObjType,
-  WeatherForecastType,
-  WeatherTodayType,
-} from '~/types/apis';
+import type {WeatherForecastType, WeatherTodayType} from '~/types/apis';
 
 export type WeatherContextType = {
   displayLocation: string;
@@ -36,20 +32,20 @@ export type WeatherContextType = {
 export const WeatherContext =
   createContextId<WeatherContextType>('weather-context');
 
-interface SuccessResponse {
+export interface SuccessResponse {
   displayLocation: string;
   forecast: WeatherForecastType['daily'];
   weatherToday: WeatherTodayType;
 }
 
-interface ErrorResponse {
+export interface ErrorResponse {
   error: {
     name: string;
     message: string;
   };
 }
 
-type WeatherResource = SuccessResponse | ErrorResponse | undefined;
+export type WeatherResource = SuccessResponse | ErrorResponse | undefined;
 
 export default component$(() => {
   const weatherContextObj = useStore({
@@ -83,16 +79,8 @@ export default component$(() => {
     const abortController = new AbortController();
     cleanup(() => abortController.abort('cleanup'));
 
-    return getWeather(weatherContextObj.query, abortController);
+    return useWeather(weatherContextObj.query, abortController);
   });
-
-  /* truthy temporary overrides */
-  // const error = 'This is an error.  ';
-  // const isLoading = true;
-
-  /* falsey temporary overrides */
-  // const error = '';
-  // const isLoading = false;
 
   return (
     <div class="weather-app">
@@ -129,80 +117,6 @@ export default component$(() => {
     </div>
   );
 });
-
-export async function getWeather(
-  query: string,
-  controller: AbortController
-): Promise<WeatherResource> {
-  function convertToFlag(countryCode: string) {
-    const codePoints = countryCode
-      .toUpperCase()
-      .split('')
-      .map((char) => 127397 + char.charCodeAt(0));
-    return String.fromCodePoint(...codePoints);
-  }
-
-  try {
-    const data = {
-      displayLocation: '',
-      forecast: {},
-      weatherToday: {},
-    };
-
-    if (/,\s/.test(query) && query.length < 4) {
-      throw new Error(`Please enter the city name (only) or a zip code.`);
-    }
-
-    // 1) Get Geo Location
-    const res = await fetch(
-      `https://geocoding-api.open-meteo.com/v1/search?name=${query}&count=10&language=en&format=json`,
-      {signal: controller.signal}
-    );
-    if (!res.ok) {
-      throw new Error('Something went wrong when fetching the geo location.');
-    }
-    const geoJSON: GeocodingObjType = await res.json();
-
-    // Open Meteo specific failure state
-    if (!geoJSON.results) {
-      throw new Error(`“${query}” not found.`);
-    }
-
-    const {latitude, longitude, timezone, name, admin1, country_code} =
-      geoJSON.results[0];
-
-    data.displayLocation = `${name}${admin1 ? `, ${admin1}` : ''} ${convertToFlag(country_code)}`;
-
-    // 2) Get forecast
-    const forecastRes = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&temperature_unit=fahrenheit&forecast_days=8&daily=weathercode,temperature_2m_max,temperature_2m_min`,
-      {signal: controller.signal}
-    );
-    if (!forecastRes.ok) {
-      throw new Error('Something went wrong when fetching the forecast.');
-    }
-    const forecastData = await forecastRes.json();
-    data.forecast = forecastData.daily;
-
-    // 3) Get today's weather
-    const todayRes = await fetch(
-      `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&temperature_unit=fahrenheit&forecast_days=2&hourly=weathercode,temperature_2m,temperature_2m&daily=temperature_2m_max,temperature_2m_min`,
-      {signal: controller.signal}
-    );
-    if (!todayRes.ok) {
-      throw new Error('Something went wrong when fetching the forecast.');
-    }
-    const todayData = await todayRes.json();
-    data.weatherToday = todayData;
-
-    console.log('data', data);
-    console.log('calling', query);
-
-    return data as SuccessResponse;
-  } catch (error) {
-    return error as ErrorResponse;
-  }
-}
 
 export const head: DocumentHead = {
   title: 'Check the Weather',
